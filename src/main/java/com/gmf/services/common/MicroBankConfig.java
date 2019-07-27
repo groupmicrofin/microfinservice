c0b2d6ecf9a2695bf25306ee633bb1f883c7bca3
Manish <manishpatel708@gmail.com>
2019-07-27 10:32:51 +0530

    common configuration

---
+++ src/main/java/com/gmf/services/common/MicroBankConfig.java
@@ -0,0 +1,8 @@
+package com.gmf.services.common;
+
+public class MicroBankConfig {
+
+    //public static final String DB_URL = "jdbc:mysql://localhost:3306/micro_finance?user=root&password=password";
+    public static final String DB_URL = "jdbc:mysql://localhost:3306/micro_finance?user=root&password=manish6994";
+    public static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+}
