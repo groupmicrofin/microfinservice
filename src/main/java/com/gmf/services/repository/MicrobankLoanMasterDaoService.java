55ee9b6168528f72b5029fc264984a558db03d10
groupmicrofin <groupmicrofin@gmail.com>
2019-07-27 12:18:14 +0530

    removing target

--- src/main/java/com/gmf/services/repository/MicrobankLoanMasterDaoService.java
+++ src/main/java/com/gmf/services/repository/MicrobankLoanMasterDaoService.java
@@ -1,5 +1,6 @@
 package com.gmf.services.repository;
 
+import com.gmf.services.common.MicroBankConfig;
 import com.gmf.services.model.LoanMaster;
 
 import java.sql.*;
@@ -8,7 +9,6 @@ import java.util.List;
 
 public class MicrobankLoanMasterDaoService {
 
-    private String DB_URL = "jdbc:mysql://localhost:3306/micro_finance?user=root&password=password";
     private String createloanMasterQueary = "select id,group_master_id from loan_masters where group_master_id=? and amt_ln_balance > 0;";
     private String createInterestUpdateQuery="update loan_masters set amt_ln_balance=amt_ln_balance+1, amt_int_accr = amt_int_accr + 1where group_master_id=1 and id=1;";
 
@@ -26,7 +26,7 @@ public class MicrobankLoanMasterDaoService {
         System.out.println("first try and catch method complete");
         Connection conn=null;
         try {
-            conn = DriverManager.getConnection(DB_URL);
+            conn = DriverManager.getConnection(MicroBankConfig.DB_URL);
             Statement prestmt = conn.createStatement();
             ResultSet rs = prestmt.executeQuery(createloanMasterQueary);
             while (rs.next()) {
@@ -67,7 +67,7 @@ public class MicrobankLoanMasterDaoService {
         System.out.println("first try and catch method complete");
 
         try {
-            conn=DriverManager.getConnection(DB_URL);
+            conn=DriverManager.getConnection(MicroBankConfig.DB_URL);
             System.out.println("input taken");
             statement=conn.createStatement();
             ResultSet rs=statement.executeQuery(createInterestUpdateQuery);
